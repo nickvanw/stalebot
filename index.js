ce2239a804a6ffdc197994360ba60f757365793f
Brandon Keepers <bkeepers@github.com>
2018-01-25 16:05:04 -0500

    Horrible first pass at getting escalation

--- index.js
+++ index.js
@@ -1,9 +1,75 @@
 const createScheduler = require('probot-scheduler');
+const moment = require('moment')
 
 module.exports = (robot) => {
-  scheduler = createScheduler(robot);
+  createScheduler(robot)
 
-  robot.on('schedule.repository', context => {
+  robot.on('schedule.repository', async context => {
+    context.log(context.repo(), 'Checking for issues that need excalation')
+
+    const issues = context.github.issues.getForRepo(context.repo({
+      state: 'open',
+      label: 'stalebot/waiting-for/maintainer',
+      per_page: 100
+    }))
+
+    await context.github.paginate(issues, async res => {
+      res.data.forEach(async issue => {
+        context.log({issue}, 'Checking issue for escalation')
+
+        let lastAuthorComment
+
+        const comments = await context.github.issues.getComments(
+          context.repo({
+            number: issue.number,
+            per_page: 100
+          })
+        )
+
+        await context.github.paginate(comments, async (res, stop) => {
+          res.data.forEach(comment => {
+            if (comment.user.id === issue.user.id) {
+              lastAuthorComment = comment
+              stop()
+            }
+          })
+        })
+
+        let currentLabel = issue.labels.find(label => label.name.startsWith('stalebot/status'))
+        let newLabel
+
+        const now = new Date()
+
+        if (lastAuthorComment) {
+          const lastCommentAt = moment(lastAuthorComment.create_at)
+          const age = now - lastCommentAt / 1000 / 60 // FIXME: switch to days :)
+
+          if (age >= 180) {
+            newLabel = 'stalebot/status/dire'
+          } else if (age >= 65) {
+            newLabel = 'stalebot/status/needs-attention'
+          } else if (age >= 5) {
+            newLabel = 'stalebot/status/stale'
+          } else {
+            newLabel = 'stalebot/status/fresh'
+          }
+
+          if (currentLabel && currentLabel.name !== newLabel) {
+            await context.github.issues.removeLabel(context.repo({
+              number: issue.number,
+              name: currentLabel.name
+            }))
+          } else if (currentLabel && newLabel === currentLabel.name) {
+            return
+          }
+
+          await context.github.issues.addLabels(context.repo({
+            number: issue.number,
+            labels: [newLabel]
+          }))
+        }
+      })
+    })
   })
 
   robot.on(['pull_request.opened', 'issues.opened'], async context => {
