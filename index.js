b654dd6d6b7cf6cc7fe4fb3e0a82274358e13517
Ashley Ellis Pierce <anellis12@gmail.com>
2018-01-25 13:22:32 -0500

    Add labels to new issues and prs

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 module.exports = (robot) => {
-  // New Issue or Pull Request is opened in the repo
   robot.on(['pull_request.opened', 'issues.opened'], async context => {
-    // Apply the labels (waiting for maintainer, ...)
-    robot.log(context)
+    const params = newActionParams(context)
+    const result = await context.github.issues.addLabels(params)
+    robot.log(result)
+    return result
   })
+
+  function newActionParams(context) {
+
+    params = {
+      owner: context["payload"]["repository"]["owner"]["login"],
+      repo: context["payload"]["repository"]["name"],
+      number: issueOrPRNumber(context),
+      labels: ["stalebot/waiting-for/maintainer"]
+    }
+    return params
+  }
+
+  function issueOrPRNumber(context) {
+    if (context["payload"]["issue"]) {
+      return context["payload"]["issue"]["number"]
+    } else {
+      return number = context["payload"]["pull_request"]["number"]
+    }
+  }
 
   // App is installed on a repo
   robot.on('installation_repositories.added', async context => {
